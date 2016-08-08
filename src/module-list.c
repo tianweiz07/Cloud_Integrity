166fd37631f7d552bc879b96211514b2c3481deb
root <root@u24be051e4e11576d7365.ant.amazon.com>
2016-08-08 22:54:16 +0000

    add support for module list

--- src/module-list.c
+++ src/module-list.c
@@ -4,6 +4,7 @@ int introspect_module_list(char *name) {
     vmi_instance_t vmi;
     addr_t next_module, list_head;
     char *modname = NULL;
+    unicode_string_t *us = NULL;
 
     if (vmi_init(&vmi, VMI_XEN | VMI_INIT_COMPLETE, name) == VMI_FAILURE) {
         printf("Failed to init LibVMI library.\n");
@@ -15,7 +16,18 @@ int introspect_module_list(char *name) {
     /**
      * get the head of the module list
      */
-    vmi_read_addr_ksym(vmi, "modules", &next_module);
+
+    switch(vmi_get_ostype(vmi)) {
+        case VMI_OS_LINUX:
+            vmi_read_addr_ksym(vmi, "modules", &next_module);
+            break;
+        case VMI_OS_WINDOWS:
+            vmi_read_addr_ksym(vmi, "PsLoadedModuleList", &next_module);
+            break;
+        default:
+            goto exit;
+    }
+
 
     list_head = next_module;
 
@@ -30,9 +42,26 @@ int introspect_module_list(char *name) {
             break;
         }
 
-        modname = vmi_read_str_va(vmi, next_module + 16, 0);
-        printf("%s\n", modname);
-        free(modname);
+        switch(vmi_get_ostype(vmi)) {
+            case VMI_OS_LINUX:
+                modname = vmi_read_str_va(vmi, next_module + 16, 0);
+                printf("%s\n", modname);
+                free(modname);
+                break;
+            case VMI_OS_WINDOWS:
+                us = vmi_read_unicode_str_va(vmi, next_module + 0x58, 0);
+                unicode_string_t out = { 0 };
+                if (us && VMI_SUCCESS == vmi_convert_str_encoding(us, &out, "UTF-8")) {
+                    printf("%s\n", out.contents);
+                    free(out.contents);
+                }
+                if (us)
+                    vmi_free_unicode_str(us);
+                break;
+            default:
+                goto exit;
+        }
+
         next_module = tmp_next;
     }
 
--- src/process-list.c
+++ src/process-list.c
@@ -14,31 +14,35 @@ int introspect_process_list (char *name) {
     vmi_pause_vm(vmi);
 
     /**
-     * get offsets of the linux kernel data structures from the sysmap 
+     * get offsets of the kernel data structures
+     * get the head of the task_struct 
      */
-    if (vmi_get_ostype(vmi) == VMI_OS_LINUX) {
-        tasks_offset = vmi_get_offset(vmi, "linux_tasks");
-        name_offset = vmi_get_offset(vmi, "linux_name");
-        pid_offset = vmi_get_offset(vmi, "linux_pid");
-    } else if (vmi_get_ostype(vmi) == VMI_OS_WINDOWS) {
-        tasks_offset = vmi_get_offset(vmi, "win_tasks");
-        name_offset = vmi_get_offset(vmi, "win_pname");
-        pid_offset = vmi_get_offset(vmi, "win_pid");
+
+    switch(vmi_get_ostype(vmi)) {
+        case VMI_OS_LINUX:
+            tasks_offset = vmi_get_offset(vmi, "linux_tasks");
+            name_offset = vmi_get_offset(vmi, "linux_name");
+            pid_offset = vmi_get_offset(vmi, "linux_pid");
+
+            list_head = vmi_translate_ksym2v(vmi, "init_task") + tasks_offset;
+
+            break;
+        case VMI_OS_WINDOWS:
+            tasks_offset = vmi_get_offset(vmi, "win_tasks");
+            name_offset = vmi_get_offset(vmi, "win_pname");
+            pid_offset = vmi_get_offset(vmi, "win_pid");
+
+            list_head = vmi_translate_ksym2v(vmi, "PsActiveProcessHead");
+
+            break;
+        default:
+            goto exit;
     }
+
 
     if (tasks_offset == 0 || pid_offset == 0 || name_offset == 0) {
         printf("Failed to find offsets\n");
         goto exit;
-    }
-
-
-    /** 
-     * get the head of the task_struct 
-     */
-    if (vmi_get_ostype(vmi) == VMI_OS_LINUX) {
-        list_head = vmi_translate_ksym2v(vmi, "init_task") + tasks_offset;
-    } else if (vmi_get_ostype(vmi)) {
-        list_head = vmi_translate_ksym2v(vmi, "PsActiveProcessHead");
     }
 
     next_list_entry = list_head;
