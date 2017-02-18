23268a6b13025a638e19fb5975b7b06f30bcc58d
tianweiz07 <artdream07@gmail.com>
2017-02-18 16:36:00 -0500

    network-check: list TCP connections

--- src/network-check.c
+++ src/network-check.c
@@ -3,70 +3,34 @@
 int introspect_network_check(char *name)
 {
     vmi_instance_t vmi;
-    addr_t init_net_addr, pde_addr, name_addr, tcp_addr, show_addr;
-    addr_t stext, etext;
-    char *filename = NULL;
-    int got_tcp = 0;
 
     if (vmi_init(&vmi, VMI_XEN | VMI_INIT_COMPLETE, name) == VMI_FAILURE) {
         printf("Failed to init LibVMI library.\n");
         return 1;
     }
 
-    /**
-     * /proc/network structure offsets. 
-     * These offset values can be retrieved by running findproc tool inside the guest OS
-     */
-    unsigned long procnet_offset = 0x38;
-    unsigned long subdir_offset = 0x50;
-    unsigned long name_offset = 0x8;
-    unsigned long next_offset = 0x40;
-    unsigned long data_offset = 0x58;
-    unsigned long show_offset = 0xf8;
+    addr_t tcp_hashinfo_addr;
+    addr_t node_addr;
 
-    /**
-     * get init_net address
-     */
-    init_net_addr = vmi_translate_ksym2v(vmi, "init_net");
+    uint16_t sport;
 
-    /**
-     * get /proc/network address
-     */
-    vmi_read_addr_va(vmi, init_net_addr+procnet_offset, 0, &pde_addr);
-    vmi_read_addr_va(vmi, pde_addr + subdir_offset, 0, &pde_addr);
+    unsigned long hlistOffset = 0x40;
+    unsigned long hlistLength = 0x10;
+    unsigned long firstOffset = 0x8;
+    unsigned long sportOffset = 0x28c;
+    unsigned long nextOffset = 0x0;
 
-    /**
-     * interate all the directories inside the /proc/network until getting the tcp directory
-     */
-    do {
-        vmi_read_addr_va(vmi, pde_addr + name_offset, 0, &name_addr);
-        filename = vmi_read_str_va(vmi, name_addr, 0);
-        if (!strncmp(filename, "tcp", sizeof("tcp"))) {
-                got_tcp = 1;
-                break;
+    tcp_hashinfo_addr = vmi_translate_ksym2v(vmi, "tcp_hashinfo");
+
+    int i;
+    for (i=0; i<32; i++) {
+        vmi_read_addr_va(vmi, tcp_hashinfo_addr+hlistOffset+i*hlistLength+firstOffset, 0, &node_addr);
+        while (!((unsigned long)node_addr & 1)) {
+            vmi_read_16_va(vmi, node_addr+sportOffset, 0, &sport);
+            uint16_t port = ((sport & 0xFF) << 8) + (sport >> 8);
+            printf("%" PRIu16 "\n",port);
+            vmi_read_addr_va(vmi, node_addr+nextOffset, 0, &node_addr);
         }
-        vmi_read_addr_va(vmi, pde_addr + next_offset, 0, &pde_addr);
-    } while (pde_addr);
-
-    if (!got_tcp)
-        goto exit;
-
-    /**
-     * get the show function address
-     */
-    vmi_read_addr_va(vmi, pde_addr + data_offset, 0, &tcp_addr);
-    vmi_read_addr_va(vmi, tcp_addr + show_offset, 0, &show_addr);
-    
-    /**
-     * get the kernel function boundary
-     */
-    stext = vmi_translate_ksym2v(vmi, "_stext");
-    etext = vmi_translate_ksym2v(vmi, "_etext");
-    
-    if (show_addr < stext || show_addr > etext) {
-        printf("TCP4 seq_ops show has been changed to 0x%x\n", (unsigned int)show_addr);
-    } else {
-        printf("TCP4 seq_ops show is not changed\n");
     }
 
 exit:
