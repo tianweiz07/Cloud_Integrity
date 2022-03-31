1d1fd68055ed5005fadf737ce4ffbeac6f2a2d1e
adinamariav <adinamariav22@gmail.com>
2022-03-31 11:33:33 +0300

    Fixed compilation errors in module-list

--- src/module-list.c
+++ src/module-list.c
@@ -1,7 +1,6 @@
 #include "vmi.h"
 
 int introspect_module_list(char *name) {
-    vmi_instance_t vmi;
     addr_t next_module, list_head;
     char *modname = NULL;
     addr_t offset;
@@ -12,10 +11,39 @@ int introspect_module_list(char *name) {
 
     unicode_string_t *us = NULL;
 
-    if (vmi_init(&vmi, VMI_XEN | VMI_INIT_COMPLETE, name) == VMI_FAILURE) {
+    vmi_instance_t vmi = NULL;
+    vmi_init_data_t *init_data = NULL;
+    uint8_t init = VMI_INIT_DOMAINNAME | VMI_INIT_EVENTS, config_type = VMI_CONFIG_GLOBAL_FILE_ENTRY;
+    void *input = NULL, *config = NULL;
+    vmi_init_error_t *error = NULL;
+
+    vmi_mode_t mode;
+    if (VMI_FAILURE == vmi_get_access_mode(NULL, name, VMI_INIT_DOMAINNAME| VMI_INIT_EVENTS, init_data, &mode)) {
+        printf("Failed to find a supported hypervisor with LibVMI\n");
+        return 1;
+    }
+
+    /* initialize the libvmi library */
+    if (VMI_FAILURE == vmi_init(&vmi, mode, name, VMI_INIT_DOMAINNAME | VMI_INIT_EVENTS, init_data, NULL)) {
         printf("Failed to init LibVMI library.\n");
         return 1;
     }
+
+    if ( VMI_PM_UNKNOWN == vmi_init_paging(vmi, 0) ) {
+        printf("Failed to init determine paging.\n");
+        vmi_destroy(vmi);
+        return 1;
+    }
+
+    if ( VMI_OS_UNKNOWN == vmi_init_os(vmi, VMI_CONFIG_GLOBAL_FILE_ENTRY, config, error) ) {
+        printf("Failed to init os.\n");
+        vmi_destroy(vmi);
+        return 1;
+    }
+
+
+    printf("LibVMI init succeeded!\n");
+
 
     vmi_pause_vm(vmi);
 
