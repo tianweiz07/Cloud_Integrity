5eff3119d66da5af708d6784c1ac696972b8ee11
adinamariav <adinamariav22@gmail.com>
2022-03-31 17:29:11 +0300

    Added new syscall test

--- tests/syscall/syscall.c
+++ tests/syscall/syscall.c
@@ -1,22 +1,54 @@
-/* execve.c */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/syscall.h>
+#include <sys/file.h>
+#include <sys/sendfile.h>
+#include <sys/random.h>
 #include <sys/types.h>
-#include <signal.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+ 
+#define BUFFER_SIZE 6710
 
-int main(int argc, char *argv[])
-{
-    printf("pid = %d\n", getpid());
-    printf("getpid = %d\n", SYS_getpid);
-    printf("getpgid = %d\n", SYS_getpgid);
-    pid_t pid, pgid;
+/* Very much inspired by https://linuxhint.com/linux_system_call_tutorial_c/ */
+ 
+int main() {
+ 
+    int fOut, fIn;
+ 
     while (1) {
-        pid = syscall(SYS_getpid, NULL, NULL);
-        pgid = syscall(SYS_getpgid, pid, NULL);
-        syscall(SYS_chmod, "/home/tianwez/aaa", 0444);
+        printf("\nI/O test with sendfile() and related system calls.\n\n");
+    
+        // Grab a BUFFER_SIZE buffer.
+        // The buffer will have random data in it but we don't care about that.
+        printf("Allocating buffer:                     ");
+        char *buffer = (char *) malloc(BUFFER_SIZE);
+        printf("DONE\n");
+    
+        // Write the buffer to fOut
+        printf("Writing data to first buffer:                ");
+        fOut = open("buffer1", O_RDONLY);
+        write(fOut, &buffer, BUFFER_SIZE);
+        close(fOut);
+        printf("DONE\n");
+    
+        printf("Copying data from first file to second:      ");
+        fIn = open("buffer1", O_RDONLY);
+        fOut = open("buffer2", O_RDONLY);
+        sendfile(fOut, fIn, 0, BUFFER_SIZE);
+        close(fIn);
+        close(fOut);
+        printf("DONE\n");
+    
+        printf("Freeing buffer:                              ");
+        free(buffer);
+        printf("DONE\n");
+    
+        printf("Deleting files:                              ");
+        unlink("buffer1");
+        unlink("buffer2");
+        printf("DONE\n");
     }
+
     return 0;
 }
